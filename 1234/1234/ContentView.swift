dbbd6516615dd1f2a2cbc3c663abb6bd7094c5cf
Carter Dickson <carterdxagames@gmail.com>
2022-06-20 18:18:32 -0700

    Xcode Project

---
+++ 1234/1234/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  1234
+//
+//  Created by Carter on 2022-06-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 1234/1234/_234App.swift
@@ -0,0 +1,17 @@
+//
+//  _234App.swift
+//  1234
+//
+//  Created by Carter on 2022-06-20.
+//
+
+import SwiftUI
+
+@main
+struct _234App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
